e390450ecb0eff157dc6341564e7d2f3c84f7617
BowangLan <60339191+BowangLan@users.noreply.github.com>
2022-12-12 08:11:44 -0800

    fixed hide scrollbar

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -5,11 +5,11 @@ import BottomPlayBar from "./ui/BottomPlayBar";
 
 export default function Layout({ children, absolute }) {
   return (
-    <div className="flex w-full min-w-screen max-w-screen h-full max-h-screen bg-slate-100 overflow-hidden">
+    <div className="flex items-stretch w-full min-w-screen max-w-screen h-full max-h-screen bg-slate-100 overflow-hidden">
       <Sidebar />
-      <div className="relative flex-1 max-w-screen h-full max-h-screen overflow-y-scroll flex flex-col">
+      <div className="relative min-w-[0] flex-1 max-w-screen h-full max-h-screen flex flex-col">
         <Header absolute={absolute} />
-        <main className="min-h-screen overflow-y-scroll">
+        <main className="w-full min-h-[0] flex-1 p-0 overflow-y-scroll scrollbar-hide">
           {children}
           <div className="h-20"></div>
         </main>
